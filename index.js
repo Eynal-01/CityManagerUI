fe359f269aa11922ae8e968ad1baa347457e9c41
Baxsh_cw07 <Baxsh_cw07@STHQ0127-02>
2023-12-14 17:16:43 +0400

    Login and Get method updated successfully

--- index.js
+++ index.js
@@ -1,66 +1,138 @@
 async function getData() {
-    const dataId = document.getElementById('dataId').value;
+  const dataId = document.getElementById("dataId").value;
 
-    const token = localStorage.getItem('jwtToken');
+  const token = localStorage.getItem("jwtToken");
 
-    if (!token) {
-        console.error('Token not available. Please login first.');
-        return;
+  if (!token) {
+    console.error("Token not available. Please login first.");
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `https://localhost:7145/api/Cities/${dataId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    console.log("Response status:", response.status);
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Data received:", data);
+    } else {
+      const responseText = await response.text();
+      console.error("Failed to get data. Server response:", responseText);
     }
-
-    try {
-        const response = await fetch(`https://localhost:7145/api/Cities/${dataId}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-            },
-        });
-
-        console.log('Response status:', response.status);
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log('Data received:', data);
-        } else {
-            const responseText = await response.text();
-            console.error('Failed to get data. Server response:', responseText);
-        }
-    } catch (error) {
-        console.error('An error occurred while getting data:', error);
-    }
+  } catch (error) {
+    console.error("An error occurred while getting data:", error);
+  }
 }
 
+// async function login() {
+//   debugger;
+//   const username = document.getElementById("username").value;
+//   const password = document.getElementById("password").value;
 
+//   try {
+//     const response = await fetch("https://localhost:7145/api/Auth/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         username: username,
+//         password: password,
+//       }),
+//     });
+
+//     console.log("Response status:", response.status);
+
+//     const contentType = response.headers.get("content-type");
+
+//     if (contentType && contentType.includes("JSON")) {
+//       const data = await response.json();
+//       const token = data.token;
+//       console.log("Login successful. Token:", token);
+//       localStorage.setItem("jwtToken", token);
+//     } else {
+//       const responseText = await response.text();
+//       console.error("Login failed. Server response:", responseText);
+//     }
+//   } catch (error) {
+//     console.error("An error occurred during login:", error);
+//   }
+// }
+
+
+// async function login() {
+//     debugger;
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
+  
+//     try {
+//       const response = await fetch("https://localhost:7145/api/Auth/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           username: username,
+//           password: password,
+//         }),
+//       });
+  
+//       console.log("Response status:", response.status);
+  
+//       const contentType = response.headers.get("content-type");
+  
+//       if (contentType && contentType.includes("application/json")) {
+//         const data = await response.json();
+//         const token = data.token;
+//         console.log("Login successful. Token:", token);
+//         localStorage.setItem("jwtToken", token);
+//       } else {
+//         const responseText = await response.text();
+//         console.error("Login failed. Server response:", responseText);
+//       }
+//     } catch (error) {
+//       console.error("An error occurred during login:", error);
+//     }
+//   }
+  
 async function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
+    debugger;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
     try {
-        const response = await fetch('https://localhost:7145/api/Auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password
-            }),
-        });
-
-        console.log('Response status:', response.status);
-
-        const contentType = response.headers.get('content-type');
-
-        if (contentType && contentType.includes('json')) {
-            const data = await response.json();
-            const token = data.token; 
-            console.log('Login successful. Token:', token);
-            localStorage.setItem('jwtToken', token); 
-        } else {
-            const responseText = await response.text();
-            console.error('Login failed. Server response:', responseText);
-        }
+      const response = await fetch("https://localhost:7145/api/Auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+  
+      console.log("Response status:", response.status);
+  
+      if (response.ok) {
+        const token = await response.text();
+        console.log("Login successful. Token:", token);
+        localStorage.setItem("jwtToken", token);
+      } else {
+        const responseText = await response.text();
+        console.error("Login failed. Server response:", responseText);
+      }
     } catch (error) {
-        console.error('An error occurred during login:', error);
+      console.error("An error occurred during login:", error);
     }
-}
+  }
+  
